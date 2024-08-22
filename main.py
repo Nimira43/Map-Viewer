60923aea99cc022d858b6491f3f81962b4b853d2
Nimira43 <n.rabone72@aol.com>
2024-08-22 20:44:34 +0100

    Updated

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ print('[*] Prime Number Utility')
 
 running = True
 
-#while running:
-print('\n[1] Determine if number is prime')
-print('\n[2] Determine all prime number within a set range')
-option = input('\n[?]Enter your choice: ')  
-print(option)
+while running:
+  print('\n[1] Determine if number is prime')
+  print('[2] Determine all prime number within a set range')
+  option = input('[?]Enter your choice: ')  
+  print(option)
+
+  if option == '1':
+    number = int(input('\n[*] Enter number: '))
+    prime_status = True
+    for i in range(0, number):
+      if number % i == 0:
+        prime_status = False

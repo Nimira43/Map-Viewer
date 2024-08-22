2270ccf6fc3d2b7dc0a0f6f922273cd36310c32b
Nimira43 <n.rabone72@aol.com>
2024-08-22 20:38:55 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('[*] Prime Number Utility')
+
+running = True
+
+#while running:
+print('\n[1] Determine if number is prime')
+print('\n[2] Determine all prime number within a set range')
+option = input('\n[?]Enter your choice: ')  
+print(option)

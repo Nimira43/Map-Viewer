b5fae1b074b5024d76ec9ee98102261d1660c9c4
Nimira43 <n.rabone72@aol.com>
2024-10-28 23:15:10 +0000

    Update colour theme

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ TERRAIN_URL = 'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga'
 PAINT_URL = 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png'
 
 COLOUR_RANGE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-ENTRY_BG = '#fff'
+ENTRY_BG = '#111'
 SIDE_PANEL_BG = '#eee'
 HISTORY_HOVER_COLOUR = '#aaa'
 BUTTON_COLOUR = '#ff4500'
@@ -11,7 +11,7 @@ BUTTON_HOVER_COLOUR = 'ffd700'
 
 TEXT_FONT = 'helvetica'
 TEXT_SIZE = 14
-TEXT_COLOUR = '#000'
+TEXT_COLOUR = '#ff4500'
 CLOSE_ITEM_COLOUR = '#555'
 
 map_image_path = 'images/map.png'

0350134b61bba681a795563b4030f7af4fa11d6a
Nimira43 <n.rabone72@aol.com>
2024-10-28 22:09:17 +0000

    Brougjt in images. Also defined various variables in settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+MAIN_URL = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+TERRAIN_URL = 'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga'
+PAINT_URL = 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png'
+
+COLOUR_RANGE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+ENTRY_BG = '#fff'
+SIDE_PANEL_BG = '#eee'
+HISTORY_HOVER_COLOUR = '#aaa'
+BUTTON_COLOUR = '#ff4500'
+BUTTON_HOVER_COLOUR = 'ffd700'
+
+TEXT_FONT = 'helvetica'
+TEXT_SIZE = 14
+TEXT_COLOUR = '#000'
+CLOSE_ITEM_COLOUR = '#555'
+
+map_image_path = 'images/map.png'
+paint_image_path = 'images/paint.png'
+terrain_image_path = 'images/terrain.png'

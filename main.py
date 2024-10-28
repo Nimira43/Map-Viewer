f0e1eaa32febcaccbb9667c9b5d1c8992c24a89e
Nimira43 <n.rabone72@aol.com>
2024-10-28 22:41:46 +0000

    Improve location entry and improved performance

--- main.py
+++ main.py
@@ -11,19 +11,27 @@ class App(ctk.CTk):
     self.title('Map Viewer')
     self.iconbitmap('map.ico')
 
+    self.input_string = ctk.StringVar()
     self.rowconfigure(0, weight = 1, uniform = 'a')
     self.columnconfigure(0, weight = 2, uniform = 'a')
     self.columnconfigure(1, weight = 8, uniform = 'a')
-    
-    self.map_widget = MapWidget(self)
+    self.map_widget = MapWidget(self, self.input_string)
 
     self.mainloop()
 
 class MapWidget(tkintermapview.TkinterMapView):
-  def __init__(self, parent):
+  def __init__(self, parent, input_string):
     super().__init__(master = parent)
     self.grid(row = 0, column = 1, sticky = 'nsew')
-    self.set_tile_server(TERRAIN_URL)
+
+    LocationEntry(self, input_string)
+    # self.set_tile_server(TERRAIN_URL)
+
+class LocationEntry(ctk.CTkEntry):
+  def __init__(self, parent, input_string):
+    super().__init__(master = parent, textvariable = input_string)
+    self.place(relx = 0.5, rely = 0.95, anchor = 'center')
+
 
 
 App()

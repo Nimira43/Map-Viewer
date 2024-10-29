e042d646af81dd41b6604d72c73d0eaf0ec313ea
Nimira43 <n.rabone72@aol.com>
2024-10-29 00:39:00 +0000

    Input working to find locations. Wrote error animation functions

--- main.py
+++ main.py
@@ -17,39 +17,58 @@ class App(ctk.CTk):
     self.columnconfigure(0, weight = 2, uniform = 'a')
     self.columnconfigure(1, weight = 8, uniform = 'a')
     
-    self.map_widgt = MapWidget(self, self.input_string, self.submit_location)
+    self.map_widget = MapWidget(self, self.input_string, self.submit_location)
 
     self.mainloop()
 
   def submit_location(self, event):
-    geolocator = Nominatim(user_agent = 'my_user')
+    geolocator = Nominatim(user_agent = 'NimiraTech')
     location = geolocator.geocode(self.input_string.get())
     if location:
-      self.map_widgt.set_address(location.address)
+      self.map_widget.set_address(location.address)
       self.input_string.set('')
     else:
-      print('Invalid Entry')
+      self.map_widget.location_entry.error_animation()
 
 class MapWidget(tkintermapview.TkinterMapView):
   def __init__(self, parent, input_string, submit_location):
     super().__init__(master = parent)
     self.grid(row = 0, column = 1, sticky = 'nsew')
 
-    LocationEntry(self, input_string, submit_location)
+    self.location_entry = LocationEntry(self, input_string, submit_location)
     # self.set_tile_server(TERRAIN_URL)
 
 class LocationEntry(ctk.CTkEntry):
   def __init__(self, parent, input_string, submit_location):
+    self.colour_index = 15
+    colour = COLOUR_RANGE[self.colour_index]
+    self.error = False
+
     super().__init__(
       master = parent, 
       textvariable = input_string,
       corner_radius = 0,
       border_width = 4,
       fg_color = ENTRY_BG,
-      border_color = BORDER_BG,
+      border_color = f'#f{colour}{colour}',
       text_color = TEXT_COLOUR,
       font = ctk.CTkFont(family = TEXT_FONT, size = TEXT_SIZE))
     self.place(relx = 0.5, rely = 0.95, anchor = 'center')
     self.bind('<Return>', submit_location)
+    input_string.trace('w', self.remove_error)
+
+  def error_animation(self):
+    self.error = True
+    if self.colour_index > 0:
+      self.colour_index -= 1
+      border_colour = f'f#{COLOUR_RANGE[self.colour_index]}{COLOUR_RANGE[self.colour_index]}'
+      text_colour = f'#{COLOUR_RANGE[-self.colour_index - 1]}00'
+      self.configure(border_colour = border_colour, text_colour = text_colour)
+      self.after(40, self.error_animation)
+
+  def remove_error(self, *args):
+    if self.error:
+      self.configure(border_colour = ENTRY_BG, text_color = TEXT_COLOUR)
+      self.colour_index = 15
 
 App()
